b7f596c3ce3e6e2ce77a3c42b3fad16ccc11b8c3
wpshamim <wpshamim@gmail.com>
2021-01-17 23:17:25 +0600

    add age in object

--- object.js
+++ object.js
@@ -4,7 +4,8 @@ var clientInfo = {
     ID      : 54892,
     Phone   : "01718860139",
     Address : "Jhenaidah Sadar, Jhenaidah",
-    zipcode : "7200"
+    zipcode : "7200",
+    age     : 30
 }
 
 console.log(clientInfo);
